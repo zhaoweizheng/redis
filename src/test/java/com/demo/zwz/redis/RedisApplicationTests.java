33904674a0a70a3c375cef41ccc13c4942f00b2e
zwz <zhaoweizheng66@163.com>
2021-07-28 20:51:09 +0800

    新建Redis项目

---
+++ src/test/java/com/demo/zwz/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.zwz.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
